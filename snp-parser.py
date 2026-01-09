d153c147e3eccd47483c293cef3c8d10ded3749e
Nathan Cloutier <nathanc4571@gmail.com>
2026-01-09 09:13:01 -0500

    Add some stuff with TFs

--- snp-parser.py
+++ snp-parser.py
@@ -23,10 +23,63 @@ def parseSnpFile():
     with open("gene.txt", "w") as geneFile:
         for gene in filteredGenes:
             geneFile.write(gene + "\n")
-    print("SNPs found in multiple associations:", filteredSNPs)
-    print("Length:", len(filteredSNPs))
-    print("Genes found in multiple associations:", filteredGenes)
-    print("Length:", len(filteredGenes))
+    print(
+        f"Processed {index} entries resulting in {len(filteredSNPs)} SNPs and {len(filteredGenes)} genes."
+    )
+
+
+def compareGenesToTfs():
+    genes = set()
+    with open("gene.txt") as geneFile:
+        for line in geneFile:
+            genes.add(line.strip())
+    tfs = set()
+    with open("all-tfs.tsv") as tfFile:
+        reader = csv.DictReader(tfFile, delimiter="\t")
+        index = 0
+        for row in reader:
+            tfs.add(row["Name.TF"])
+            index += 1
+            if (index % 100000) == 0:
+                print(f"Processed {index} TF entries.")
+    commonGenes = genes.intersection(tfs)
+    with open("tfs.txt", "w") as outputFile:
+        for gene in commonGenes:
+            outputFile.write(gene + "\n")
+    print(f"Found {len(commonGenes)} TFs in the gene list out of {index} total TFs.")
+
+
+def compareDiffGeneToTfs():  # Compare the differently expressed genes to the TFs that were impacted by the SNPs
+    diffGenes = set()
+    with open("gene.txt") as geneFile:
+        for line in geneFile:
+            diffGenes.add(line.strip())
+    tfs = set()
+    with open("tfs.txt") as tfFile:
+        for line in tfFile:
+            tfs.add(line.strip())
+    tfDict = {}
+    with open("all-tfs.tsv") as tfFile:
+        reader = csv.DictReader(tfFile, delimiter="\t")
+        index = 0
+        for row in reader:
+            if row["Name.TF"] in tfs:
+                tfDict[row["Name.TF"]] = tfDict.get(row["Name.TF"], []) + [
+                    row["Name.Target"]
+                ]
+            index += 1
+            if (index % 100000) == 0:
+                print(f"Processed {index} TF entries.")
+    impactedGenes = set()
+    for gene in diffGenes:
+        if any(gene in targets for targets in tfDict.values()):
+            impactedGenes.add(gene)
+    print(f"Identified {len(impactedGenes)} impacted genes from the TFs.")
+    with open("impacted-genes.txt", "w") as outputFile:
+        for gene in impactedGenes:
+            outputFile.write(gene + "\n")
 
 
 parseSnpFile()
+compareGenesToTfs()
+compareDiffGeneToTfs()
